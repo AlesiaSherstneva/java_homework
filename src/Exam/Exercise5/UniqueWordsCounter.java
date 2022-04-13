a873e05554be6869eb3d5a6b61a5cb5b091d21d1
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-04-13 15:11:53 +0300

    Add Exercise5

---
+++ src/Exam/Exercise5/UniqueWordsCounter.java
@@ -0,0 +1,26 @@
+package Exam.Exercise5;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UniqueWordsCounter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String str = scanner.nextLine();
+        Set<String> words = new HashSet<>();
+
+        Pattern pattern = Pattern.compile("\\S[\\w]*\\b");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()){
+            words.add(str.substring(matcher.start(), matcher.end()).toLowerCase(Locale.ROOT));
+        }
+
+        System.out.println("Во введённой строке " + words.size() + " уникальных слов");
+    }
+}
