e56ba75459af78226a72b2aeedc71d5cbf6890c7
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-04-14 01:50:54 +0300

    Change name

--- src/Exam/Exercise9/numbersSorter.java
+++ src/Exam/Exercise9/numbersSorter.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class NumberSorter {
+public class numbersSorter {
 
     public static void main(String[] args) throws IOException {
 
