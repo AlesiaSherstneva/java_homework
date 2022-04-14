cc327abca39d1a97230be25da056cfafc0ab7e7a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-04-14 15:32:42 +0300

    Add Exercise3, change Exercise1 and Exercise2

---
+++ src/Credit/Exercise3/Exercise3.java
@@ -0,0 +1,23 @@
+package Credit;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        int n = (int) (Math.random() * 28800);
+        System.out.println(n);
+        if (n != 0) {
+            switch (n / 3600) {
+                case 8 -> System.out.print("Осталось 8 часов");
+                case 7 -> System.out.print("Осталось 7 часов");
+                case 6 -> System.out.print("Осталось 6 часов");
+                case 5 -> System.out.print("Осталось 5 часов");
+                case 4 -> System.out.print("Осталось 4 часа");
+                case 3 -> System.out.print("Осталось 3 часа");
+                case 2 -> System.out.print("Осталось 2 часа");
+                case 1 -> System.out.print("Остался 1 час");
+                case 0 -> System.out.print("Осталось менее часа");
+            }
+        } else {
+            System.out.print("Рабочий день закончился!");
+        }
+    }
+}
