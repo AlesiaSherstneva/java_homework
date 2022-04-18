73580b62e9d4c8677538fe74e6c333874d3f277d
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-04-18 11:19:44 +0300

    Add Exercise11

---
+++ src/Credit/Exercise11/MyNewQueue.java
@@ -0,0 +1,30 @@
+package Credit.Exercise11;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MyNewQueue extends ArrayDeque<Integer> implements MyQueue {
+    private final ArrayList<Integer> maxList = new ArrayList<>();
+
+    @Override
+    public void push(Integer number) {
+        maxList.add(number);
+        Collections.sort(maxList);
+        super.push(number);
+    }
+
+    @Override
+    public Integer pop() {
+        maxList.remove(super.getFirst());
+        return super.pop();
+    }
+
+    @Override
+    public Integer max() {
+        if (maxList.isEmpty()) {
+            return null;
+        }
+        return maxList.get(maxList.size()-1);
+    }
+}
---
+++ src/Credit/Exercise11/MyQueue.java
@@ -0,0 +1,13 @@
+package Credit.Exercise11;
+
+@SuppressWarnings("UnusedReturnValue")
+public interface MyQueue {
+
+    void push(Integer a);
+
+    Integer pop();
+
+    Integer max();
+
+}
+
