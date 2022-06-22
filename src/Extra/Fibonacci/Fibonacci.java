b6874868b631f131623909f7b12c5f7eb3db13cf
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-06-22 03:59:59 +0300

    Edit the solution

--- src/Extra/Fibonacci/Fibonacci.java
+++ src/Extra/Fibonacci/Fibonacci.java
@@ -10,15 +10,16 @@ public class Fibonacci {
         long number = scanner.nextLong();
 
         long first = 0, second = 1, temp;
+
         if (number == 0) {
             System.out.println(first);
         } else if (number == 1) {
             System.out.println(second);
         } else {
-            for (int i = 1; i <= number - 2; i++) {
-                temp = first;
-                first += second;
-                second = temp;
+            for (int i = 2; i < number; i++) {
+                temp = second;
+                second += first;
+                first = temp;
             }
             System.out.println(first + second);
         }
