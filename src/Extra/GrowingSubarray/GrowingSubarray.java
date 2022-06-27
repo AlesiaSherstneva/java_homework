4557f88078b0cbc3695448b4f34cc0dcab27fd88
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-06-27 22:48:14 +0300

    Edit class's and package's names

--- src/Extra/GrowingSubarray/GrowingSubarray.java
+++ src/Extra/GrowingSubarray/GrowingSubarray.java
@@ -1,8 +1,8 @@
-package Extra.GrowingSubArray;
+package Extra.GrowingSubarray;
 
 import java.util.Arrays;
 
-public class GrowingSubArray {
+public class GrowingSubarray {
     public static void main(String[] args) {
         int[] firstArray = {2, 1, 4, 1, 2, 3, 4, 5, 6, 7, 3, 1};
         int[] secondArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
