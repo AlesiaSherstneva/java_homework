51c38a4a1a8aaabfd667b64a2a414973f3bbbebe
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-22 22:59:14 +0300

    Delete Main class

--- src/Extra/RPN_Calculator/Main.java
+++
@@ -1,9 +0,0 @@
-package Extra.RPN_Calculator;
-
-public class Main {
-    public static void main(String[] args) {
-        String expression = Calculator.expressionToRPN("2*(3+2*(1+2*(1+3)))");
-        System.out.println(expression);
-        System.out.println(Calculator.RPNToAnswer(expression));
-    }
-}
