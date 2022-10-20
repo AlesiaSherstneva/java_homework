1c9a29e78cd6f82146f11545f21f2c653d7505fd
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-20 13:59:45 +0300

    Add the solution

--- src/Extra/TreeLeafCount/Main.java
+++ src/Extra/TreeLeafCount/Main.java
@@ -2,13 +2,46 @@ package Extra.TreeLeafCount;
 
 public class Main {
     public static void main(String[] args) {
-        Node Tree =
-                new Node(20,
-                        new Node(7,
-                                new Node(4, null, new Node(6)), new Node(9)),
-                        new Node(35,
-                                new Node(31, new Node(28), null),
-                                new Node(40, new Node(38), new Node(52))));
-        System.out.println("В этом дереве " + Tree.treeLeafCount(Tree) + " листьев");
+        Node firstTree =
+                new Node(50,
+                        new Node(17,
+                                new Node(12,
+                                        new Node(9), new Node(14)),
+                                new Node(23, null, new Node(19))),
+                        new Node(72,
+                                new Node(54,
+                                        null, new Node(67)),
+                                new Node(76)));
+        System.out.println("В первом дереве " + firstTree.treeLeafCount() + " листьев");
+
+        Node secondTree =
+                new Node(9,
+                        new Node(6,
+                                new Node(3,
+                                        new Node(1,
+                                                null, new Node(2)),
+                                        new Node(4, null, new Node(5))),
+                                new Node(8, new Node(7), null)),
+                        new Node(17,
+                                new Node(16,
+                                        new Node(12,
+                                                new Node(11,
+                                                        new Node(10), null),
+                                                new Node(14,
+                                                        new Node(13), new Node(15))), null),
+                                new Node(20,
+                                        new Node(19,
+                                                new Node(18), null),
+                                        new Node(21))));
+        System.out.println("Во втором дереве " + secondTree.treeLeafCount() + " листьев");
+
+        Node thirdTree = new Node(4, null,
+                new Node(7, null,
+                        new Node(16, null,
+                                new Node(20, null,
+                                        new Node(37, null,
+                                                new Node(38, null,
+                                                        new Node(43)))))));
+        System.out.println("В третьем дереве " + thirdTree.treeLeafCount() + " листьев");
     }
 }
